90448cf5f5a9a2804bb0615e7a90774565b9c7de
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-04-17 16:37:54 -0300

    count Strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import java.util.*
+fun main() {
+    val sc = Scanner(System.`in`)
+    var space: Int = 0
+    var number: Int = 0
+    var others: Int = 0
+    println("Digite uma frase legal: ")
+    val phrase: String = sc.nextLine()
+    // usamos o toChArray para dividir nossa string em posicoes e fazer analise sobre as posicoes
+    val text = phrase.toCharArray()
+    val upPhrase = phrase.uppercase()
+    val lwPhrase = phrase.lowercase()
+    val lengthPhrase = phrase.length
+    println("Frase escolhida totalmente em letra maiuscula: -> $upPhrase <-")
+    println("Frase escolhida totalmente em letra minuscula: -> $lwPhrase <-")
+    println("Tamanho da frase contando espacos, numeros, etc... $lengthPhrase")
+    println("Imprimindo cada caracter da phrase")
+    phrase.forEach {
+        println(it)
+    }
+    for (c in text) {
+      if (c.isLetter()) {
+          others ++
+      } else if (c.isDigit()) {
+          number ++
+      } else if (c.isWhitespace()) {
+          space ++
+      }
+    }
+    println("Temos $others letras(a) na frase")
+    println("Temos $number numeros(o) na frase")
+    println("Temos $space espacos(o) na frase")
 }
